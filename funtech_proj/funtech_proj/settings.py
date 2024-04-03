1cc89c0a3ea365936e87e707014db059ea84f38f
Alex Proskuriakov <alexpro1972@yandex.ru>
2024-04-03 12:49:18 +0300

    Add version to compose file

--- funtech_proj/funtech_proj/settings.py
+++ funtech_proj/funtech_proj/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-z6yj*jdyo@m-8$btm4ji5gy#c7gbq%sm+(d=6k48xtbpj6t@f4"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG")
 
 ALLOWED_HOSTS = ["backend", "localhost"]  # type: ignore [var-annotated]
 CSRF_TRUSTED_ORIGINS = ["http://127.0.0.1", "http://localhost"]
