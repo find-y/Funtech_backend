9a30fd0f94aeba1d100b7d75b6f98c782b4334ec
unknown <ger-adv@yandex.ru>
2024-04-08 17:52:35 +0300

    Удалил лишние поля в пользовательской модели (согласия на рассылку)

--- funtech_proj/api/urls.py
+++ funtech_proj/api/urls.py
@@ -1,5 +1,6 @@
-from api.views import EventViewSet
-from django.urls import path
+from api.views import EventViewSet  # ProfileLists,
+from django.conf.urls import url
+from django.urls import include, path
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -7,13 +8,15 @@ from rest_framework.routers import DefaultRouter
 
 v1_router = DefaultRouter()
 
+
 v1_router.register("events", EventViewSet, basename="events")
+
 
 schema_view = get_schema_view(
     openapi.Info(
         title="Funtech API",
         default_version="v1",
-        description="Документация",
+        description="Документация _",
         terms_of_service="URL страницы с пользовательским соглашением",
         # contact=openapi.Contact(email="admin@kadmin.ru"),
         license=openapi.License(name="BSD License"),
@@ -23,18 +26,23 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path(
-        "swagger<format>/",
+    path("v1/", include(v1_router.urls)),
+    # path("v1/lists/", ProfileLists.as_view(), name="lists"),
+]
+
+urlpatterns += [
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
     ),
-    path(
-        "swagger/",
+    url(
+        r"^swagger/$",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
     ),
-    path(
-        "redoc/",
+    url(
+        r"^redoc/$",
         schema_view.with_ui("redoc", cache_timeout=0),
         name="schema-redoc",
     ),
