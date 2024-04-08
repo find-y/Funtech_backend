6d687cd05287a0196e4937402038b0fe2fe2d6fb
unknown <ger-adv@yandex.ru>
2024-04-08 15:13:07 +0300

    Добавил необходимые поля в кастомную модель пользователя

--- funtech_proj/users/models.py
+++ funtech_proj/users/models.py
@@ -1,13 +1,5 @@
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
-
-'''
-# внес шаблонного кастомного юзера из заготовок,
-# чтобы можно было тестировать модели ивентов
-
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
@@ -16,7 +8,18 @@ class User(AbstractUser):
         "username",
         "first_name",
         "last_name",
+        "mobilefone",
+        "workplace",
+        "position",
+        "experience",
     )
+    EXPERIENCE_CHOICES = [
+        ('', ''),
+        ('BEGINNER', 'От 1 года'),
+        ('INTERMEDIATE', 'От 3 лет'),
+        ('ADVANCED', 'ОТ 5 лет'),
+        ('Other', 'Другое'),
+    ] 
 
     email = models.EmailField(
         verbose_name="Электронная почта",
@@ -39,6 +42,30 @@ class User(AbstractUser):
         verbose_name="Пароль",
         max_length=25,
     )
+    mobilefone = models.CharField(
+        verbose_name="Мобильный телефон",
+        unique=True,
+        max_length=15,
+    )
+    workplace = models.CharField(
+        verbose_name="Место работы",
+        max_length=25,
+    )
+    position = models.CharField(
+        verbose_name="Должность",
+        unique=True,
+        max_length=15,
+    )
+    experience = models.CharField(
+        max_length=20,
+        choices=EXPERIENCE_CHOICES,
+    )
+    consert_form = models.BooleanField(
+        verbose_name="Согласие на предоставление анкеты"
+    )
+    consert_summary = models.BooleanField(
+        verbose_name="Согласие на предоставление резюме"
+    )
 
     def __str__(self):
         return f'{self.username}'
@@ -47,4 +74,4 @@ class User(AbstractUser):
         ordering = ('-date_joined',)
         verbose_name = 'Пользователь'
         verbose_name_plural = 'Пользователи'
-'''
+
