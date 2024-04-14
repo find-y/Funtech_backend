c71fc902dd9e6637155a44ee8c9c39a058b444f7
unknown <yasha1bz@gmail.com>
2024-04-14 16:23:33 +0300

    event ser save

--- funtech_proj/api/serializers.py
+++ funtech_proj/api/serializers.py
@@ -1,5 +1,8 @@
 from class_makers.api import serializer_class_maker
 from events import models as em
+
+# from rest_framework import relations, serializers
+from rest_framework.serializers import ModelSerializer
 from shared import models as sm
 from users.models import User
 
@@ -13,19 +16,19 @@ SpeakerSerializer = serializer_class_maker(em.Speaker)
 Program_partSerializer = serializer_class_maker(em.Program_part)
 ParticipantEventSerializer = serializer_class_maker(em.ParticipantEvent)
 Gallery_imageSerializer = serializer_class_maker(em.Gallery_image)
-EventSerializer = serializer_class_maker(em.Event)
+# EventSerializer = serializer_class_maker(em.Event)
 UserSerializer = serializer_class_maker(User)
 
 
-# class EventFullSerializer(EventSerializer):
-#     town = TownSerializer()
-#     form = FormSerializer()
-#     # speakers = SpeakerSerializer(many=True)
-#     # speakers = SpeakerSerializer(many=True, source="speakers_events")
-#     # gallery_images = Gallery_imageSerializer()
-#     # image = Base64ImageField(required=False, allow_null=True)
+class EventSerializer(ModelSerializer):
+    town = TownSerializer()
+    form = FormSerializer()
+    # speakers = SpeakerSerializer(many=True)
+    # speakers = SpeakerSerializer(many=True, source="speakers_events")
+    # gallery_images = Gallery_imageSerializer()
+    # image = Base64ImageField(required=False, allow_null=True)
 
-#     class Meta:
-#         model = em.Event
-#         # fields = "__all__"
-#         exclude = ("created", "org", "specialization", "stack", "participants")
+    class Meta:
+        model = em.Event
+        # fields = "__all__"
+        exclude = ("created", "org", "specializations", "stack", "participants")
